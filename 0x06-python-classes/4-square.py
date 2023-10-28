3ccaff49afc7ba750dcd833d7d25a625ea455cc7
ShahdAmmar <shahdammar413@gmail.com>
2023-10-28 12:27:56 -0700

    4-square.py commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Square Class Module """
+
+
+class Square:
+    """ Defines a Square class """
+
+    def __init__(self, size=0):
+        """ Initilization:
+        Attributes:
+            size: length of square size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Property of length of square side
+        Raises:
+            TypeError: if not int
+            ValueError: if < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """ defines area method """
+        sq_area = self.__size ** 2
+        return sq_area
