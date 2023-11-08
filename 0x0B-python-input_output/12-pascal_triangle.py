16a333788cc7f280ace191948a6c03218637b47a
ShahdAmmar <shahdammar413@gmail.com>
2023-11-07 18:19:31 -0800

    12-pascal_triangle.py commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,7 +6,6 @@ def pascal_triangle(n):
     """ represent pascal's triangles """
     if n <= 0:
         return []
-
     trians = [[1]]
     while len(trians) != n:
         trian = trians[-1]
@@ -15,4 +14,4 @@ def pascal_triangle(n):
             temp.append(trian[i] + trian[i + 1])
         temp.append(1)
         trians.append(temp)
-    return trian
+    return trians
