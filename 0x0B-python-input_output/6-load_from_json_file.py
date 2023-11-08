421ddd86bd946c5964da0be6cdf5f378b7d99e79
ShahdAmmar <shahdammar413@gmail.com>
2023-11-07 16:58:48 -0800

    6-load_from_json_file.py commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defining load_from_json_file function """
+import json
+
+
+def load_from_json_file(filename):
+    """ creates object from json string """
+    with open(filename, 'r', encoding='utf-8') as fl:
+        json.load(fl)
+       # return objectt
