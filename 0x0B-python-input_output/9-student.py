85c235c4c54ea7f86c7fdcc3ead79f31b45ead61
ShahdAmmar <shahdammar413@gmail.com>
2023-11-07 17:34:54 -0800

    9-student.py commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" defining Student Class """
+
+
+class Student:
+    """ represents student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ returns dictionary representation of a instances """
+        return self.__dict__
