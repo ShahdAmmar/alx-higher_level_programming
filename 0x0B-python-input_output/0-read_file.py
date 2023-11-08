58dc3528f35811932b8948cb4621e0af59e980f1
ShahdAmmar <shahdammar413@gmail.com>
2023-11-07 16:11:31 -0800

    0-read_file.py commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """ reads file with utf-8 """
     with open(filename, 'r', encoding='utf-8') as fl:
         read_data = fl.read()
         print(read_data, end="")
