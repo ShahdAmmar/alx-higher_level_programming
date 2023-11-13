597f9f91f172eeb1acc200024bad51b7ffb31855
ShahdAmmar <shahdammar413@gmail.com>
2023-11-13 13:58:02 -0800

    0 & 1 commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" defining Base class """
+
+
+class Base:
+    """ Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Intialization """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
