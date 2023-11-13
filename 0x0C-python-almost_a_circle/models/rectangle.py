da273ce8f371a98c1d106ccb1ba76517688a230b
ShahdAmmar <shahdammar413@gmail.com>
2023-11-13 15:34:00 -0800

    2 & 3 commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,55 +14,59 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """ width getter """
-            return self.__width
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """ width setter """
-            self.validate_number("width", value, False)
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        self.validate_number("width", value, False)
+        self.__width = value
 
-        @property
-        def height(self):
-            """ height getter """
-            return self.__height
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """ height setter """
-            self.validate_number("height", value, False)
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        self.validate_number("height", value, False)
+        self.__height = value
 
-        @property
-        def x(self):
-            """ x getter """
-            return self.__x
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """ x setter """
-            self.validate_number("x", value)
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        self.validate_number("x", value)
+        self.__x = value
 
-        @property
-        def y(self):
-            """ y getter """
-            return self.__y
+    @property
+    def y(self):
+        """ y getter """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """ y setter """
-            self.validate_number("y", value)
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        self.validate_number("y", value)
+        self.__y = value
 
-        def validate_number(self, attrb, value, xy=True):
-            """ checks if value is valid or not """
-            if type(value) != int:
-                raise TypeError("{} must be an integer".format(attrb))
-            elif xy and value < 0:
-                raise ValueError("{} must be >= 0".format(attrb))
-            elif not xy and value <= 0:
-                raise ValueError("{} must be > 0".format(attrb))
+    def validate_number(self, attrb, value, xy=True):
+        """ checks if value is valid or not """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attrb))
+        if xy and value < 0:
+            raise ValueError("{} must be >= 0".format(attrb))
+        elif not xy and value <= 0:
+            raise ValueError("{} must be > 0".format(attrb))
+
+    def area(self):
+        """ calculate area """
+        return self.width * self.height
