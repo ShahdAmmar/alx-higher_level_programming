d2475d14df473fa026fc7325e4f77b2f38416e4e
ShahdAmmar <shahdammar413@gmail.com>
2023-11-27 13:07:50 -0800

    3-is_kind_of_class.py commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" is_kind_of_class Module """
+
+
+def is_kind_of_class(obj, a_class):
+    """ defining function """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
