6bafc998fbd2830c837180516412cd3586a4db7d
ShahdAmmar <shahdammar413@gmail.com>
2023-12-14 15:49:08 -0800

    7 & 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- list score and name
-SELECT score, name FROM second_tbale HAVING name IS NOT NULL ORDER by score DESC;
+SELECT score, name FROM second_tbale HAVING name IS NOT NULL ORDER BY score DESC;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- insert new row into table first_table
-INSERT INTO first_table (id, name) VALUES (89, "Best Schhol");
+INSERT INTO first_table VALUES (89, "Best Schhol");
