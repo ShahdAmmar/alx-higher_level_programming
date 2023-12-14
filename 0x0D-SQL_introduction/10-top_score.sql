4ff5aa1a714d8df0a4268056093a3cb390463731
ShahdAmmar <shahdammar413@gmail.com>
2023-12-14 15:16:26 -0800

    tasks 4-10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- list records of second_table ordered by score
+SELECT score, name from second_table ORDER BY score;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- creta table
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
