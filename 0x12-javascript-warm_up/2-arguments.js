5c5c4e6a0497e41736c5844474fbed88c53aaba6
ShahdAmmar <shahdammar413@gmail.com>
2024-01-08 15:56:19 -0800

    2-3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const argvlen = process.argv.length;
+
 if (argvlen === 2) {
 	console.log('No argument');
-}
-else if (argvlen === 3) {
+} else if (argvlen === 3) {
 	console.log('Argument found');
-}
-else {
+} else {
 	console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const argvlst = process.argv
+
 if (argvlst[2]) {
 	console.log(argvlst[2]);
-}
-else {
+} else {
 	console.log('No argument');
 }
