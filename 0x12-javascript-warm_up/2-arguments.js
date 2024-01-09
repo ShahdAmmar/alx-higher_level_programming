57f7c475e952dbc2ae556070729efdbfb19df02f
ShahdAmmar <shahdammar413@gmail.com>
2024-01-08 16:06:15 -0800

    3 style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const argvlen = process.argv.length;
 if (argvlen === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argvlen === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
