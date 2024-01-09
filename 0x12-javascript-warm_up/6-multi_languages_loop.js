6aad0bdd7a2b9c33c8bcd9632d3563acbdb668ba
ShahdAmmar <shahdammar413@gmail.com>
2024-01-08 16:38:51 -0800

    6-8 styles

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const strlst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i< strlst.length; i++) {
+for (let i = 0; i < strlst.length; i++) {
   console.log(strlst[i]);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 const size = parseInt(process.argv[2]);
+x = 'X';
+
 if (size) {
   for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+    console.log(x.repeat(size));
     }
-} else {
+}else {
   console.log('Missing size');
 }
