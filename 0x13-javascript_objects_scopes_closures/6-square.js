683d8c03cb5c1fd78bb904073e04619094ace354
ShahdAmmar <shahdammar413@gmail.com>
2024-02-19 10:48:11 -0800

    6-square commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const SSquare = require('./5-square');
+
+class Square extends SSquare {
+  charPrint (c) {
+    if (c == null) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
